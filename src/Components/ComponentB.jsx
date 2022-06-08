cfc84b4225befc047aa7a618081702a10ce4b12a
sachinrawagent <sa147831@gmail.com>
2022-06-08 20:29:17 +0530

    redux folder created

--- src/Components/ComponentB.jsx
+++ src/Components/ComponentB.jsx
@@ -1,25 +1,15 @@
 import React, { useState } from 'react'
 
 function ComponentB() {
-    const [form,setForm]=useState({
-        name:""
-    })
     const handlechange=(e)=>{
-      const {className,value}=e.target;
-      setForm({
-          ...form,[className]:value,
-      })
-    }
-    const handlesubmit=(e)=>{
-        e.preventDefault();
-        console.log(form);
+       
     }
   return (
     <div>
-        <form onSubmit={handlesubmit}>
-        <input type="text" onChange={handlechange} value={form.name} className="name" />
-        <input type="submit"/>
-        </form>
+      
+        <input type="text" onChange={handlechange}  className="name" />
+        
+        
     </div>
   )
 }
